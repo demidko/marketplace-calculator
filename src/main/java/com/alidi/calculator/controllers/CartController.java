21eb5a6ce3dd4384fa83336764dbd1f227c7d87d
Daniil Demidko <demidko@farpost.com>
2021-12-03 04:16:45 +1000

    Настроил обработку ошибок

--- src/main/java/com/alidi/calculator/controllers/CartController.java
+++ src/main/java/com/alidi/calculator/controllers/CartController.java
@@ -1,12 +1,17 @@
 package com.alidi.calculator.controllers;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 import com.alidi.calculator.calculators.CartPriceCalculator;
 import com.alidi.calculator.dto.input.UnpricedCartView;
 import com.alidi.calculator.dto.output.PricedCartView;
 import com.alidi.calculator.models.cart.UnpricedCart;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * валидация на уровне типов, уже осуществляемая spring-boot'ом за нас. Входящие dto раскладываются
  * в удобные модели и отправляются для расчетов дальше по этапу в недра бизнес логики.
  */
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class CartController {
@@ -33,9 +39,28 @@ public class CartController {
    * работу сервисов.
    */
   @PostMapping("/calculate-cart-price")
-  public PricedCartView calculatePrice(@NonNull UnpricedCartView unpricedCartView) {
+  public PricedCartView calculatePrice(UnpricedCartView unpricedCartView) {
+    log.info("New request: {}", unpricedCartView);
     var unpricedCart = UnpricedCart.fromView(unpricedCartView);
     var pricedCart = cartPriceCalculator.calculateCart(unpricedCart);
     return PricedCartView.fromPricedCart(pricedCart);
+  }
+
+  @ExceptionHandler(NullPointerException.class)
+  @ResponseStatus(BAD_REQUEST)
+  public void invalidParameterValue(NullPointerException e) {
+    log.warn("Invalid parameter value", e);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  @ResponseStatus(BAD_REQUEST)
+  public void invalidParameterValue(IllegalArgumentException e) {
+    log.warn("Invalid parameter value", e);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseStatus(INTERNAL_SERVER_ERROR)
+  public void otherErrors(RuntimeException e) {
+    log.error("Request processing error", e);
   }
 }
